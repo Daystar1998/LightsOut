6c17dddba9b59c413d9ba650af07529efae03ac5
Matthew Day <Daystar.1998@yahoo.com>
2019-08-28 22:20:03 -0500

    Add LightsOut project

---
+++ LightsOut/AboutForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LightsOut {
+	partial class AboutForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label1.Location = new System.Drawing.Point(97, 32);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(157, 31);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Lights Out!";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::LightsOut.Properties.Resources.Light_Bulb;
+			this.pictureBox1.Location = new System.Drawing.Point(55, 32);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(36, 36);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(100, 63);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(104, 16);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "By Matthew Day";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(25, 102);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(270, 16);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Turn all the lights out (black) to win the game!";
+			// 
+			// button1
+			// 
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(118, 131);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "OK";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// AboutForm
+			// 
+			this.AcceptButton = this.button1;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(322, 166);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AboutForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "About";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ LightsOut/MainForm.cs
@@ -0,0 +1,227 @@
+﻿// Matthew Day
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut {
+	public partial class MainForm : Form {
+
+		private const int GridOffset = 25; // Distance from upper-left side of window
+		private const int GridBottomOffset = 5; // Distance from buttons at bottom of window
+		private int gridLength = 200; // Size in pixels of grid
+		private int numCells = 3; // Number of cells in grid
+		private int cellLength;
+
+		private bool[,] grid; // Stores on/off state of cells in grid
+		private Random rand; // Used to generate random numbers
+
+		public MainForm() {
+
+			InitializeComponent();
+
+			rand = new Random(); // Initializes random number generator
+
+			createGrid();
+
+			gridLength = newGameButton.Location.Y - GridOffset - GridBottomOffset;
+
+			cellLength = gridLength / numCells;
+		}
+
+		private void MainForm_Paint(object sender, PaintEventArgs e) {
+
+			gridLength = newGameButton.Location.Y - GridOffset - GridBottomOffset;
+
+			cellLength = gridLength / numCells;
+
+			Graphics g = e.Graphics;
+
+			for (int r = 0; r < numCells; r++) {
+
+				for (int c = 0; c < numCells; c++) {
+
+					// Get proper pen and brush for on/off
+					// grid section
+					Brush brush;
+					Pen pen;
+
+					if (grid[r, c]) {
+
+						pen = Pens.Black;
+						brush = Brushes.White; // On
+					} else {
+
+						pen = Pens.White;
+						brush = Brushes.Black; // Off
+					}
+
+					// Determine (x,y) coord of row and col to draw rectangle
+					int x = c * cellLength + GridOffset;
+					int y = r * cellLength + GridOffset;
+
+					// Draw outline and inner rectangle
+					g.DrawRectangle(pen, x, y, cellLength, cellLength);
+					g.FillRectangle(brush, x + 1, y + 1, cellLength - 1, cellLength - 1);
+				}
+			}
+		}
+
+		private void MainForm_MouseDown(object sender, MouseEventArgs e) {
+
+			// Make sure click was inside the grid
+			if (e.X < GridOffset || e.X > cellLength * numCells + GridOffset ||
+			e.Y < GridOffset || e.Y > cellLength * numCells + GridOffset)
+				return;
+
+			// Find row, col of mouse press
+			int r = (e.Y - GridOffset) / cellLength;
+			int c = (e.X - GridOffset) / cellLength;
+
+			// Invert selected box and all surrounding boxes
+			for (int i = r - 1; i <= r + 1; i++) {
+
+				for (int j = c - 1; j <= c + 1; j++) {
+
+					if (i >= 0 && i < numCells && j >= 0 && j < numCells) {
+
+						grid[i, j] = !grid[i, j];
+					}
+				}
+			}
+
+			// Redraw grid
+			this.Invalidate();
+
+			// Check to see if puzzle has been solved
+			if (PlayerWon()) {
+
+				// Display winner dialog box
+				MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
+				MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		private bool PlayerWon() {
+
+			bool result = true;
+
+			for (int r = 0; r < numCells; r++) {
+
+				for (int c = 0; c < numCells; c++) {
+
+					if (grid[r, c]) {
+
+						result = false;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private void NewGameButton_Click(object sender, EventArgs e) {
+
+			// Fill grid with either white or black
+			for (int r = 0; r < numCells; r++) {
+
+				for (int c = 0; c < numCells; c++) {
+
+					grid[r, c] = rand.Next(2) == 1;
+				}
+			}
+
+			// Redraw grid
+			this.Invalidate();
+		}
+
+		private void NewToolStripMenuItem_Click(object sender, EventArgs e) {
+
+			NewGameButton_Click(sender, e);
+		}
+
+		private void ExitToolStripMenuItem_Click(object sender, EventArgs e) {
+
+			Close();
+		}
+
+		private void ExitButton_Click(object sender, EventArgs e) {
+
+			ExitToolStripMenuItem_Click(sender, e);
+		}
+
+		private void AboutToolStripMenuItem_Click(object sender, EventArgs e) {
+
+			AboutForm aboutBox = new AboutForm();
+			aboutBox.ShowDialog(this);
+		}
+
+		private void MainForm_Resize(object sender, EventArgs e) {
+
+			// Redraw grid
+			this.Invalidate();
+		}
+
+		private void X3ToolStripMenuItem_Click(object sender, EventArgs e) {
+
+			// Set checked states for menu items
+			x3ToolStripMenuItem.Checked = true;
+			x4ToolStripMenuItem.Checked = false;
+			x5ToolStripMenuItem.Checked = false;
+
+			numCells = 3;
+			createGrid();
+
+			// Redraw grid
+			this.Invalidate();
+		}
+
+		private void X4ToolStripMenuItem_Click(object sender, EventArgs e) {
+
+			// Set checked states for menu items
+			x3ToolStripMenuItem.Checked = false;
+			x4ToolStripMenuItem.Checked = true;
+			x5ToolStripMenuItem.Checked = false;
+
+			numCells = 4;
+			createGrid();
+
+			// Redraw grid
+			this.Invalidate();
+		}
+
+		private void X5ToolStripMenuItem_Click(object sender, EventArgs e) {
+
+			// Set checked states for menu items
+			x3ToolStripMenuItem.Checked = false;
+			x4ToolStripMenuItem.Checked = false;
+			x5ToolStripMenuItem.Checked = true;
+
+			numCells = 5;
+			createGrid();
+
+			// Redraw grid
+			this.Invalidate();
+		}
+
+		private void createGrid() {
+
+			grid = new bool[numCells, numCells];
+
+			// Turn entire grid on
+			for (int r = 0; r < numCells; r++) {
+
+				for (int c = 0; c < numCells; c++) {
+
+					grid[r, c] = true;
+				}
+			}
+		}
+	}
+}
